6d7890dda656fa87ba89454414f212f90f7f58db
Matt Steed <73540952+MattSteedWork@users.noreply.github.com>
2022-05-04 11:23:34 +0100

    V2

--- SFBM.sh
+++ SFBM.sh
@@ -24,11 +24,14 @@ echo -e "\n\e[32;1m[+] Nmap Output Written to $inn-Results/Nmap/$inn-Nmap.txt\n"
 if grep -Fq '445/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
   echo -e "\n\e[32;1m[+] Grabbing SMB Shares...\n";
   smbclient -L $inn
-else
+elif grep -Fq '80/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
   echo -e "[+] No SMB Here :(\n";
   echo -e "\e[32;1m[+]Running Wfuzz...\n"
   wfuzz -c -v --hc 404 -w dsmall.txt -u http://$inn:80/FUZZ -f $inn-Results/Wfuzz/$inn-80-Wfuzz.txt
   echo -e "\e[32;1m[+] Output Written to $inn-Results/Wfuzz/$inn-80-Wuzz.txt\n";
+else
+  echo -e "[+] No SMB or HTTP/HTTPS Here :("
+  echo ""
 fi
 exit
 }
