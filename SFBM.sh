b73a8be07f6f29c35449571d3e95ff1d84c77a1e
Matt Steed <73540952+MattSteedWork@users.noreply.github.com>
2022-05-04 11:15:05 +0100

    V1

---
+++ SFBM.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+echo -e "\e[32;1m####################################################"
+echo -e "\e[32;1m#                                                  #"
+echo -e "\e[32;1m#\e[31;1m              @ SUPERFUZZ BIGMUFF @               \e[32;1m#"
+echo -e "\e[32;1m#\e[31;1m           NMAP/SMB/WFUZZ Info Grabber            \e[32;1m#"
+echo -e "\e[32;1m#                                                  #"
+echo -e "\e[32;1m####################################################"
+echo""
+
+echo -e "\e[32;1m[+] Enter IP to scan:"
+echo ""
+echo -e "\e[31;1m"
+read inn
+echo ""
+echo -e "\e[32;1m[+] Running Nmap..."
+function map(){
+mkdir $inn-Results
+mkdir $inn-Results/Nmap
+mkdir $inn-Results/SMB
+mkdir $inn-Results/Wfuzz
+nmap $inn -v0 -Pn -o $inn-Results/Nmap/$inn-Nmap.txt
+echo -e "\n\e[32;1m[+] Nmap Output Written to $inn-Results/Nmap/$inn-Nmap.txt\n"
+if grep -Fq '445/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
+  echo -e "\n\e[32;1m[+] Grabbing SMB Shares...\n";
+  smbclient -L $inn
+else
+  echo -e "[+] No SMB Here :(\n";
+  echo -e "\e[32;1m[+]Running Wfuzz...\n"
+  wfuzz -c -v --hc 404 -w dsmall.txt -u http://$inn:80/FUZZ -f $inn-Results/Wfuzz/$inn-80-Wfuzz.txt
+  echo -e "\e[32;1m[+] Output Written to $inn-Results/Wfuzz/$inn-80-Wuzz.txt\n";
+fi
+exit
+}
+map
+
