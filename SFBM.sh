f37ca8ffc3d34a9b924035a53cb13b659bc16033
meepmaster360 <paulomotaborges@gmail.com>
2022-05-05 10:04:11 +0100

    1

--- SFBM.sh
+++ SFBM.sh
@@ -25,6 +25,7 @@ echo ""
 echo -e "${GREEN}[+] Running Nmap...${NOCOLOR}"
 
 function mk_dir() {
+ ### Problem with re-writhing same directory...
 mkdir $inn-Results
 mkdir $inn-Results/Nmap
 mkdir $inn-Results/SMB
@@ -35,14 +36,17 @@ mkdir $inn-Results/Wfuzz
 function SFBM() {
 nmap $inn -v0 -Pn -o $inn-Results/Nmap/$inn-Nmap.txt
 echo -e "\n${GREEN}[+] Nmap Output Written to $inn-Results/Nmap/$inn-Nmap.txt\n"
+
 if grep -Fq '445/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
   echo -e "\n${GREEN}[+] Grabbing SMB Shares...\n";
   smbclient -L $inn
   elif grep -Fq '80/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
   echo -e "[+] No SMB Here :(\n";
+
   echo -e "${GREEN}[+]Running Wfuzz...\n"
   wfuzz -c -v --hc 404 -w dsmall.txt -u http://$inn:80/FUZZ -f $inn-Results/Wfuzz/$inn-80-Wfuzz.txt
   echo -e "${GREEN}[+] Output Written to $inn-Results/Wfuzz/$inn-80-Wuzz.txt\n";
+
   else
   echo -e "[+] No SMB or HTTP/HTTPS Here :("
   echo ""
