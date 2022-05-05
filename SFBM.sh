6aa346467136bcc299d0f70f5f961612cbf2ec61
meepmaster360 <paulomotaborges@gmail.com>
2022-05-05 09:35:25 +0100

    1

--- SFBM.sh
+++ SFBM.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
+# Update: 05-05-2022
+# Description: Update Upgrade
+
+# Color variables 
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
 clear
-echo -e "\e[32;1m####################################################"
-echo -e "\e[32;1m#                                                  #"
-echo -e "\e[32;1m#\e[31;1m              @ SUPERFUZZ BIGMUFF @               \e[32;1m#"
-echo -e "\e[32;1m#\e[31;1m           NMAP/SMB/WFUZZ Info Grabber            \e[32;1m#"
-echo -e "\e[32;1m#                                                  #"
-echo -e "\e[32;1m####################################################"
+echo -e "${GREEN}####################################################"
+echo -e "${GREEN}#                                                  #"
+echo -e "${GREEN}#${RED}              @ SUPERFUZZ BIGMUFF @               ${GREEN}#"
+echo -e "${GREEN}#${RED}           NMAP/SMB/WFUZZ Info Grabber            ${GREEN}#"
+echo -e "${GREEN}#                                                  #"
+echo -e "${GREEN}####################################################"
 echo""
 
-echo -e "\e[32;1m[+] Enter IP to scan:"
+echo -e "${GREEN}[+] Enter IP to scan:"
 echo ""
-echo -e "\e[31;1m"
+echo -e "${RED}"
 read inn
 echo ""
-echo -e "\e[32;1m[+] Running Nmap..."
-function map(){
+echo -e "${GREEN}[+] Running Nmap..."
+
+function mk_dir() {
 mkdir $inn-Results
 mkdir $inn-Results/Nmap
 mkdir $inn-Results/SMB
 mkdir $inn-Results/Wfuzz
+
+function SFBM() {
 nmap $inn -v0 -Pn -o $inn-Results/Nmap/$inn-Nmap.txt
-echo -e "\n\e[32;1m[+] Nmap Output Written to $inn-Results/Nmap/$inn-Nmap.txt\n"
+echo -e "\n${GREEN}[+] Nmap Output Written to $inn-Results/Nmap/$inn-Nmap.txt\n"
 if grep -Fq '445/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
-  echo -e "\n\e[32;1m[+] Grabbing SMB Shares...\n";
+  echo -e "\n${GREEN}[+] Grabbing SMB Shares...\n";
   smbclient -L $inn
-elif grep -Fq '80/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
+  elif grep -Fq '80/tcp' $inn-Results/Nmap/$inn-Nmap.txt; then
   echo -e "[+] No SMB Here :(\n";
-  echo -e "\e[32;1m[+]Running Wfuzz...\n"
+  echo -e "${GREEN}[+]Running Wfuzz...\n"
   wfuzz -c -v --hc 404 -w dsmall.txt -u http://$inn:80/FUZZ -f $inn-Results/Wfuzz/$inn-80-Wfuzz.txt
-  echo -e "\e[32;1m[+] Output Written to $inn-Results/Wfuzz/$inn-80-Wuzz.txt\n";
-else
+  echo -e "${GREEN}[+] Output Written to $inn-Results/Wfuzz/$inn-80-Wuzz.txt\n";
+  else
   echo -e "[+] No SMB or HTTP/HTTPS Here :("
   echo ""
 fi
+
 exit
+
 }
-map
+
+# Call functions
+
+SFBF
 
